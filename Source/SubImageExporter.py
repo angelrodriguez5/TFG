e9538f23def4b289654a865d3d4206bc69a6f534
Angel <angel.rodryguez@gmail.com>
2019-06-27 13:22:43 +0200

    Added 2-click region selection

--- Source/SubImageExporter.py
+++ Source/SubImageExporter.py
@@ -7,9 +7,6 @@ import os
 from tkinter import Tk
 from tkinter.filedialog import askopenfilename
 
-# Config constants
-REGION_SIZE = (7,5)
-
 # List of chosen points
 marks = []
 
@@ -20,23 +17,25 @@ pixelMode = True
 class Mark(patches.Rectangle):
 
     DEFAULT_COLOR = 'b'
+    DEFAULT_SIZE = (7,5)
 
     ''' Instance attributes
         - center
         - classNum
+        - customSize
     '''
 
-    def __init__(self, center, classNum):
-        # Default, 1 pixel square that shadows a pixel of an image
-        # Pyplot prints the pixel (0,0) of an image around the point (0,0) of the plot
-        # so we need to offset the position of the mark to properly aling with the pixel in the image
+    def __init__(self, center, classNum, customSize = None):
+        # Create patch
+        super().__init__(center, 1, 1)
+
         self.center = center
-        self.classNum = classNum
-        xy = self.calcPointXY()
-        super().__init__(xy, 1, 1)
-        
+        self.classNum = classNum    
+        self.customSize = customSize  
         # Update the view to match current mode
         self.updateView()
+
+
 
     def getCenter(self):
         return self.center
@@ -53,7 +52,11 @@ class Mark(patches.Rectangle):
 
         else:
             # Draw a box around the center
-            (width, height) = REGION_SIZE
+            if (self.customSize):
+                width, height = self.customSize
+            else:
+                width, height = self.DEFAULT_SIZE
+
             self.set_xy(self.calcBoxXY())
             self.set_width(width)
             self.set_height(height)
@@ -63,6 +66,8 @@ class Mark(patches.Rectangle):
             self.set_linewidth(1)
 
     def calcPointXY(self):
+        # Pyplot prints the pixel (0,0) of an image around the point (0,0) of the plot
+        # so we need to offset the position of the mark to properly aling with the pixel in the image
         # Offset xy so the square is drawn over the center pixel
         newX = self.center[0] - .5
         newY = self.center[1] - .5
@@ -70,15 +75,23 @@ class Mark(patches.Rectangle):
 
     def calcBoxXY(self):
         # Calculate the position of the corner of the box taking into account the offset
-        newX = self.center[0] - math.floor(REGION_SIZE[0] / 2) - .5
-        newY = self.center[1] - math.floor(REGION_SIZE[1] / 2) - .5
+        if (self.customSize):
+            newX = self.center[0] - math.floor(self.customSize[0] / 2) - .5
+            newY = self.center[1] - math.floor(self.customSize[1] / 2) - .5
+        else:
+            newX = self.center[0] - math.floor(self.DEFAULT_SIZE[0] / 2) - .5
+            newY = self.center[1] - math.floor(self.DEFAULT_SIZE[1] / 2) - .5
         return (newX, newY)
 
     def normalise(self, imgDimensions):
         # Return a tuple with the class number, center coordinates and dimensions
         # normalised between 0 and 1 with respect to the size of an image
         cx, cy = map(lambda x,y: x / (y - 1), self.center, imgDimensions)
-        h, w = map(lambda x,y: x / (y - 1), REGION_SIZE, imgDimensions)
+        if (self.customSize):
+            h, w = map(lambda x,y: x / (y), self.customSize, imgDimensions)
+        else:
+            h, w = map(lambda x,y: x / (y), self.DEFAULT_SIZE, imgDimensions)
+
         return (self.classNum, cx, cy, h, w)
 
 #endregion
@@ -91,8 +104,9 @@ class State(object):
         raise NotImplementedError
 
 
-class AddState(State):
+class AddPointState(State):
 
+    # Click on center
     def processClick(self, event, classNum):
         # Reference to the subplot clicked
         ax = event.inaxes
@@ -106,12 +120,81 @@ class AddState(State):
             return
         except StopIteration as error:
             # if it is not found, add it to the mark list
+            # Create mark at center point with default size
             mark = Mark(xy, classNum)
             marks.append(mark)
             # And to the figure
             ax.add_patch(mark)
             print ("New Mark added at: %s" % (xy,))
 
+class AddRegionState(State):
+    ''' Instance attributes:
+        -secondClick
+        -topLeft
+    '''
+    def __init__(self):
+        self.secondClick = False
+        self.topLeft = None
+
+    # Click on top left and bottom right to select a region
+    def processClick(self, event, classNum):
+        # Round event data to get the pixel
+        # The pixel (0,0) of an image is plotted between (-0.5, -0.5) and (0.5, 0.5)
+        xy = (int(round(event.xdata)), int(round(event.ydata)))
+        # If it is the second click then calculate the mark position
+        # and add it to the list
+        if (self.secondClick):
+            self.secondClick = False
+            print ("Second click at: %s" % (xy,))
+            # Get center of the region based on two clicks
+            cxy = self.calculateCenter(self.topLeft, xy)
+            size = self.calculateSize(self.topLeft, xy)
+            try:
+                # Try to find the mark in the list
+                # PROBLEM with concentric regions of different size
+                mark = next(x for x in marks if x.getCenter() == cxy)
+                # If it is found return
+                return
+            except StopIteration as error:
+                # if it is not found, add mark with custom size to the list
+                mark = Mark(cxy, classNum, size)
+                marks.append(mark)
+            
+                # Reference to the subplot clicked
+                ax = event.inaxes
+                # And to the figure
+                ax.add_patch(mark)
+                print ("New Mark added at: %s" % (cxy,))
+        else:
+            # If it is the first click, store its position and prepare for second click
+            self.topLeft = xy
+            self.secondClick = True
+            print ("New region top left at: %s" % (xy,))
+
+    def calculateCenter(self, topLeft, bottomRight):
+        left, top = topLeft
+        right, bot = bottomRight
+        # Check that pixels are positioned correctly
+        if (top > bot or left > right):
+            raise Exception
+
+        # Calculate center pixel favouring bottom and right for pair length sections
+        cx = math.ceil((left + right) / 2)
+        cy = math.ceil((top + bot) / 2)
+
+        return (cx,cy)
+
+    def calculateSize(self, topLeft, bottomRight):
+        left, top = topLeft
+        right, bot = bottomRight
+        # Check that pixels are positioned correctly
+        if (top > bot or left > right):
+            raise Exception
+
+        width = math.floor(right - left + 1)
+        height = math.floor(bot - top + 1)
+
+        return (width, height)
 
 class MoveState(State):
 
@@ -140,9 +223,10 @@ class DeleteState(State):
 
 class GUI(object):
 
-    ADD_STATE = 0
-    MOV_STATE = 1
-    DEL_STATE = 2
+    ADD_CENTER_STATE = 0
+    ADD_REGION_STATE = 1
+    MOV_STATE = 2
+    DEL_STATE = 3
 
     '''Instance attributes
         -imgPath
@@ -172,6 +256,20 @@ class GUI(object):
     
     def onClick(self, event):
         classNum = 0
+        # Check that click is in bounds
+        height, width, channels = self.img.shape
+        if (event is None):
+            print('Click outside of canvas')
+            return
+
+        if (event.xdata < -0.5 or
+            event.xdata > width - 0.5 or
+            event.ydata < -0.5 or
+            event.ydata > height - 0.5):
+
+            print ('Click out of bounds')
+            return
+        
         # Delegate click management to the state
         self.state.processClick(event, classNum)
         # Update figure
@@ -182,15 +280,18 @@ class GUI(object):
         key = event.key
         # Map keys to actions
         if   (key == "1"):
-            self.setState(self.ADD_STATE)
+            self.setState(self.ADD_CENTER_STATE)
 
         elif (key == "2"):
-            self.setState(self.MOV_STATE)
+            self.setState(self.ADD_REGION_STATE)
 
         elif (key == "3"):
-            self.setState(self.DEL_STATE)
+            self.setState(self.MOV_STATE)
 
         elif (key == "4"):
+            self.setState(self.DEL_STATE)
+
+        elif (key == "0"):
             self.exportRegions()
 
         elif (key == "º"):
@@ -204,9 +305,13 @@ class GUI(object):
 
     def setState(self, code):
         # Change title and state acording to the code
-        if (code == self.ADD_STATE):
-            plt.title("Add")
-            self.state = AddState()
+        if (code == self.ADD_CENTER_STATE):
+            plt.title("Add point")
+            self.state = AddPointState()
+
+        elif (code == self.ADD_REGION_STATE):
+            plt.title("Add region")
+            self.state = AddRegionState()
 
         elif (code == self.MOV_STATE):
             plt.title("Move")
---
+++ Source/Util.py
@@ -0,0 +1,7 @@
+class Singleton(object):
+    __instance = None
+    def __new__(cls, val):
+        if Singleton.__instance is None:
+            Singleton.__instance = object.__new__(cls)
+        Singleton.__instance.val = val
+        return Singleton.__instance
