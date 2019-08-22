29cfca5beec8f263e74323b8ca53e629c23d6fe0
Angel <angel.rodryguez@gmail.com>
2019-08-22 12:31:24 +0200

    experiment xtttv_pretrained

--- Yolo/train.py
+++ Yolo/train.py
@@ -25,7 +25,7 @@ import torch.optim as optim
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--experiment_name", type=str, default="pretrained_tttvx", help="name of the folder to save logs, checkpoints...")
+    parser.add_argument("--experiment_name", type=str, default="pretrained_xtttv", help="name of the folder to save logs, checkpoints...")
     parser.add_argument("--epochs", type=int, default=250, help="number of epochs")
     parser.add_argument("--batch_size", type=int, default=1, help="size of each image batch")
     parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
