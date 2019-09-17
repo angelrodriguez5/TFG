742ee7b205c82182719f9081ef133ed51a065e57
Angel <angel.rodryguez@gmail.com>
2019-09-17 13:14:40 +0200

    refactor detect.py

--- detect.py
+++ detect.py
@@ -23,10 +23,10 @@ from matplotlib.ticker import NullLocator
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--image_folder", type=str, default="data/test_img", help="path to dataset")
-    parser.add_argument("--model_def", type=str, default="config/customModelDef.cfg", help="path to model definition file")
-    parser.add_argument("--weights_path", type=str, default="currentWeights.pth", help="path to weights file")
-    parser.add_argument("--class_path", type=str, default="data/custom/classes.names", help="path to class label file")
+    parser.add_argument("--image_folder", type=str, default="Yolo/data/test_img", help="path to dataset")
+    parser.add_argument("--model_def", type=str, default="Yolo/config/customModelDef.cfg", help="path to model definition file")
+    parser.add_argument("--weights_path", type=str, default="Yolo/currentWeights.pth", help="path to weights file")
+    parser.add_argument("--class_path", type=str, default="Yolo/data/custom/classes.names", help="path to class label file")
     parser.add_argument("--conf_thres", type=float, default=0.8, help="object confidence threshold")
     parser.add_argument("--nms_thres", type=float, default=0.4, help="iou thresshold for non-maximum suppression")
     parser.add_argument("--batch_size", type=int, default=1, help="size of the batches")
