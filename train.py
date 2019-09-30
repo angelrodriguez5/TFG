eb727f395ffdb4a455cfaca202483f1b734eb71b
Angel <angel.rodryguez@gmail.com>
2019-09-30 11:59:16 +0200

    refactor

--- train.py
+++ train.py
@@ -29,8 +29,8 @@ if __name__ == "__main__":
     parser.add_argument("--epochs", type=int, default=700, help="number of epochs")
     parser.add_argument("--batch_size", type=int, default=4, help="size of each image batch")
     parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
-    parser.add_argument("--model_def", type=str, default="Yolo/config/customModelDef.cfg", help="path to model definition file")
-    parser.add_argument("--data_config", type=str, default="Yolo/config/custom.data", help="path to data config file")
+    parser.add_argument("--model_def", type=str, default="config/customModelDef.cfg", help="path to model definition file")
+    parser.add_argument("--data_config", type=str, default="config/custom.data", help="path to data config file")
     parser.add_argument("--pretrained_weights", type=str, help="if specified starts from checkpoint model") # default="weights/darknet53.conv.74"
     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
