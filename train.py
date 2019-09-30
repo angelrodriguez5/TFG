b1a4a58946a7c6f8fd3f5ef69520b77a7dc7df62
Angel <angel.rodryguez@gmail.com>
2019-09-30 15:09:28 +0200

    experiment

--- train.py
+++ train.py
@@ -25,13 +25,13 @@ import torch.optim as optim
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("--experiment_name", type=str, default="tttvx_np1000_lre-3", help="name of the folder to save logs, checkpoints...")
-    parser.add_argument("--epochs", type=int, default=700, help="number of epochs")
+    parser.add_argument("--experiment_name", type=str, default="tttvx_p400_lre-3", help="name of the folder to save logs, checkpoints...")
+    parser.add_argument("--epochs", type=int, default=400, help="number of epochs")
     parser.add_argument("--batch_size", type=int, default=2, help="size of each image batch")
     parser.add_argument("--gradient_accumulations", type=int, default=2, help="number of gradient accums before step")
     parser.add_argument("--model_def", type=str, default="config/customModelDef.cfg", help="path to model definition file")
     parser.add_argument("--data_config", type=str, default="config/custom.data", help="path to data config file")
-    parser.add_argument("--pretrained_weights", type=str, help="if specified starts from checkpoint model") # default="weights/darknet53.conv.74"
+    parser.add_argument("--pretrained_weights", type=str, default="Yolo/weights/darknet53.conv.74", help="if specified starts from checkpoint model") # default="weights/darknet53.conv.74"
     parser.add_argument("--n_cpu", type=int, default=8, help="number of cpu threads to use during batch generation")
     parser.add_argument("--img_size", type=int, default=416, help="size of each image dimension")
     parser.add_argument("--checkpoint_interval", type=int, default=10, help="interval between saving model weights")
