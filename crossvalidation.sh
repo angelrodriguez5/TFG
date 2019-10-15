0a5dd8599eeb2c5927fef417bb6623dadfc2ab87
Angel <angel.rodryguez@gmail.com>
2019-10-15 19:00:37 +0200

    crossvalidation script finished

--- crossvalidation.sh
+++ crossvalidation.sh
@@ -1,9 +1,27 @@
 echo Experiment 1 - tttvx
 
 cp data/crossvalidation/1-tttvx/* data/custom/
-python3 train.py --experiment_name tttvx_test1 --epochs 1
+python3 train.py --experiment_name tttvx_150 --epochs 150
 
 echo Experiment 2 - xtttv
 
 cp data/crossvalidation/2-xtttv/* data/custom/
-python3 train.py --experiment_name xtttv_test1 --epochs 1
+python3 train.py --experiment_name xtttv_150 --epochs 150
+
+echo Experiment 3 - vxttt
+
+cp data/crossvalidation/3-vxttt/* data/custom/
+python3 train.py --experiment_name vxttt_150 --epochs 150
+
+echo Experiment 4 - tvxtt
+
+cp data/crossvalidation/4-tvxtt/* data/custom/
+python3 train.py --experiment_name tvxtt_150 --epochs 150
+
+echo Experiment 5 - ttvxt
+
+cp data/crossvalidation/5-ttvxt/* data/custom/
+python3 train.py --experiment_name ttvxt_150 --epochs 150
+
+# Leave dataset as with its default values
+cp data/crossvalidation/1-tttvx/* data/custom/
