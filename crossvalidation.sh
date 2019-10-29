316da9754ae08dce18fb5d600dfecb52d2df99c1
Angel <angel.rodryguez@gmail.com>
2019-10-29 11:57:45 +0100

    modified crossvalidation script to accept parameters

--- crossvalidation.sh
+++ crossvalidation.sh
@@ -1,22 +1,25 @@
+NAME=$1
+PARAMS=$2
+
 echo Experiment 1 - tttvx
 cp data/crossvalidation/1-tttvx/* data/custom/
-python3 train.py --experiment_name tttvx_150 --epochs 150
+python3 train.py --experiment_name tttvx_150_$NAME --epochs 150 $PARAMS
 
 echo Experiment 2 - xtttv
 cp data/crossvalidation/2-xtttv/* data/custom/
-python3 train.py --experiment_name xtttv_150 --epochs 150
+python3 train.py --experiment_name xtttv_150_$NAME --epochs 150 $PARAMS
 
 echo Experiment 3 - vxttt
 cp data/crossvalidation/3-vxttt/* data/custom/
-python3 train.py --experiment_name vxttt_150 --epochs 150
+python3 train.py --experiment_name vxttt_150_$NAME --epochs 150 $PARAMS
 
 echo Experiment 4 - tvxtt
 cp data/crossvalidation/4-tvxtt/* data/custom/
-python3 train.py --experiment_name tvxtt_150 --epochs 150
+python3 train.py --experiment_name tvxtt_150_$NAME --epochs 150 $PARAMS
 
 echo Experiment 5 - ttvxt
 cp data/crossvalidation/5-ttvxt/* data/custom/
-python3 train.py --experiment_name ttvxt_150 --epochs 150
+python3 train.py --experiment_name ttvxt_150_$NAME --epochs 150 $PARAMS
 
 # Leave dataset as with its default values
 cp data/crossvalidation/1-tttvx/* data/custom/
