11667516e11479ae58d1bec2e7e9c41cbe0c398d
Angel <angel.rodryguez@gmail.com>
2019-11-08 14:42:35 +0100

    debug

--- multi_analyse.sh
+++ multi_analyse.sh
@@ -4,12 +4,12 @@ MODEL="/home/angel/experiments/Pretrained/exp1_xtttv/checkpoints/"
 VIDEO="--video_path /home/angel/HET-CAM/DSC_1098"
 MAX="--max_x 1500" # 25 s
 
-WEIGTHS="--weight_path ${MODEL}yolov3_ckpt_40.pth"
+WEIGTHS="--weights_path ${MODEL}yolov3_ckpt_40.pth"
 python3 analyse.py $WEIGTHS $VIDEO $MAX --output_name xtttv_40
 
-WEIGTHS="--weight_path ${MODEL}yolov3_ckpt_80.pth"
+WEIGTHS="--weights_path ${MODEL}yolov3_ckpt_80.pth"
 python3 analyse.py $WEIGTHS $VIDEO $MAX --output_name xtttv_80
 
-WEIGTHS="--weight_path ${MODEL}yolov3_ckpt_120.pth"
+WEIGTHS="--weights_path ${MODEL}yolov3_ckpt_120.pth"
 python3 analyse.py $WEIGTHS $VIDEO $MAX --output_name xtttv_120
 
