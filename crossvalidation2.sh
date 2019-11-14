28cc3985bc0131dfaa99b42ed685b41ece68c7ad
Angel <angel.rodryguez@gmail.com>
2019-11-14 17:25:17 +0100

    temporary

--- crossvalidation2.sh
+++ crossvalidation2.sh
@@ -8,20 +8,20 @@ PARAMS2="--augmentation False"
 # python3 train.py --experiment_name exp1_tttvx $PARAMS1
 # python3 train.py --experiment_name exp2_tttvx $PARAMS2
 
-echo Experiment 2 - xtttv
-cp data/crossvalidation/2-xtttv/* data/custom/
-python3 train.py --experiment_name exp1_xtttv $PARAMS1
-python3 train.py --experiment_name exp2_xtttv $PARAMS2
+# echo Experiment 2 - xtttv
+# cp data/crossvalidation/2-xtttv/* data/custom/
+# python3 train.py --experiment_name exp1_xtttv $PARAMS1
+# python3 train.py --experiment_name exp2_xtttv $PARAMS2
 
-echo Experiment 3 - vxttt
-cp data/crossvalidation/3-vxttt/* data/custom/
-python3 train.py --experiment_name exp1_vxttt $PARAMS1
-python3 train.py --experiment_name exp2_vxttt $PARAMS2
+# echo Experiment 3 - vxttt
+# cp data/crossvalidation/3-vxttt/* data/custom/
+# python3 train.py --experiment_name exp1_vxttt $PARAMS1
+# python3 train.py --experiment_name exp2_vxttt $PARAMS2
 
-echo Experiment 4 - tvxtt
-cp data/crossvalidation/4-tvxtt/* data/custom/
-python3 train.py --experiment_name exp1_tvxtt $PARAMS1
-python3 train.py --experiment_name exp2_tvxtt $PARAMS2
+# echo Experiment 4 - tvxtt
+# cp data/crossvalidation/4-tvxtt/* data/custom/
+# python3 train.py --experiment_name exp1_tvxtt $PARAMS1
+# python3 train.py --experiment_name exp2_tvxtt $PARAMS2
 
 echo Experiment 5 - ttvxt
 cp data/crossvalidation/5-ttvxt/* data/custom/
@@ -33,9 +33,9 @@ cp data/crossvalidation/1-tttvx/* data/custom/
 
 # Organise experiments in folders
 cd /home/angel/experiments
-mkdir $NAME1
+# mkdir $NAME1
 mv exp1* $NAME1
-mkdir $NAME2
+# mkdir $NAME2
 mv exp2* $NAME2
 
 # Logs to dropbox
